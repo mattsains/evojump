86c5edd5fc071c08274190cb1130cece1b06bc5d
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-08-18 20:32:11 +0200

    Added solution to repository

---
+++ LevelServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
